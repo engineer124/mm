3e8b558bbcc3eb2036f58c3153ad862d70db0d31
engineer124 <engineer124engineer124@gmail.com>
2021-10-03 16:19:55 +1100

    More voice work

--- src/libultra/voice/voicecrc.c
+++ src/libultra/voice/voicecrc.c
@@ -1,33 +1,33 @@
 #include "global.h"
 
-s32 __osVoiceContDataCrc(u8* arg0, u32 arg1) {
-    u32 phi_a0;
-    u32 phi_v0;
-    u32 phi_v1 = 0;
+u8 __osVoiceContDataCrc(u8* data, u32 arg1) {
+    u32 bit;
+    u32 byte;
+    u32 ret = 0;
 
-    for (phi_v0 = arg1; phi_v0 > 0; phi_v0--) {
-        for (phi_a0 = 0x80; phi_a0 > 0; phi_a0 >>= 1) {
-            phi_v1 <<= 1;
-            if (*arg0 & phi_a0) {
-                if (phi_v1 & 0x100) {
-                    phi_v1 ^= 0x84;
+    for (byte = arg1; byte > 0; byte--) {
+        for (bit = 0x80; bit > 0; bit >>= 1) {
+            ret <<= 1;
+            if (*data & bit) {
+                if (ret & 0x100) {
+                    ret ^= 0x84;
                 } else {
-                    phi_v1++;
+                    ret++;
                 }
-            } else if (phi_v1 & 0x100) {
-                phi_v1 ^= 0x85;
+            } else if (ret & 0x100) {
+                ret ^= 0x85;
             }
         }
-        arg0++;
+        data++;
     }
 
     do {
-        phi_v1 <<= 1;
-        if (phi_v1 & 0x100) {
-            phi_v1 ^= 0x85;
+        ret <<= 1;
+        if (ret & 0x100) {
+            ret ^= 0x85;
         }
-        phi_v0++;
-    } while (phi_v0 < 8);
+        byte++;
+    } while (byte < 8);
 
-    return phi_v1 & 0xFF;
+    return ret;
 }
